01ade78552b74c80a39e605776b3d8df97e88a31
LMO GerritBot <gerrit@libremobileos.com>
2024-02-09 04:09:51 +0000

    Adapt to LMODroid by device-add script

--- lmodroid_dopinder.mk
+++ lmodroid_dopinder.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/google/atv/products/atv_base.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_tv.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -19,7 +19,7 @@ PRODUCT_DEVICE := dopinder
 PRODUCT_GMS_CLIENTID_BASE := android-askey-tv
 PRODUCT_MANUFACTURER := askey
 PRODUCT_MODEL := sti6140d360
-PRODUCT_NAME := lineage_dopinder
+PRODUCT_NAME := lmodroid_dopinder
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=dopinder \
